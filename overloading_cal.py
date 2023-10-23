70e54b28ef5f656cb79bf04ff19cd45c6bbad984
Akansha-1705 <63223995+Akansha-1705@users.noreply.github.com>
2023-10-23 09:10:12 +0530

    Update overloading_cal.py

--- overloading_cal.py
+++ overloading_cal.py
@@ -12,3 +12,31 @@ def calci(a,b):
 
 calci(2)
 calci(3,5)
+
+
+
+
+overloading -- calculator
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+
+    def add(self, a, b, c):
+        return a + b + c
+
+
+    def subtract(self, a, b):
+        return a - b
+
+
+    def multiply(self, a, b):
+        return a * b
+
+
+# Usage
+calc = Calculator()
+print(calc.add(2, 3))       # Overloaded add method with 2 arguments
+print(calc.add(2, 3, 4))    # Overloaded add method with 3 arguments
+print(calc.subtract(5, 2))
+print(calc.multiply(4, 6))
