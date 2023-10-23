6ea81ec5aebc767b01d4c5fee1b07ab828e22666
Akansha-1705 <63223995+Akansha-1705@users.noreply.github.com>
2023-10-23 06:20:12 +0530

    Update abstraction.py

--- abstraction.py
+++ abstraction.py
@@ -45,3 +45,47 @@ print(d.display_details())
     
         
     
+
+
+
+from abc import ABC, abstractmethod
+class Book(ABC):
+def init__(self, title, quantity, author, price):
+self.title = title
+self.quantity = quantity
+self.author = author
+self. price = price
+self. discount = None
+
+def set discount(self, discount):
+self discount = discount
+
+def get price(self):
+if self discount:
+return self price* (1-self.____discount)
+return self price
+@abstractmethod
+
+def repr_(self):
+return "Book: (self.title), Quantity: (self.quantity), Author: (self.author), Price: (self.get_price())"
+
+class Novel(Book):
+def init__(self, title, quantity, author, price, pages):
+super(). init (title, quantity, author, price)
+self-pages = pages
+
+def repr (self):
+return f"Book: (self.title), Quantity: {self.quantity), Author: {self.author), Price: {self.get_price()}"
+
+class Academic(Book):
+def ____init__(self, title, quantity, author, price, branch): super(). init (title, quantity, author, price)
+self.branch branch
+
+def repr (self):
+return "Book: [self.title), Branch: (self.branch), Quantity: (self.quantity), Author: (self.author), Price:
+(self.get price()}"
+novel1 = Novel('Two States', 20, 'Chetan Bhagat', 200, 187)
+novel1.set_discount(0.20)
+academic1 = Academic("Python Foundations', 12, 'PSF', 655, 'IT')
+print(novel1)
+print(academic1)
